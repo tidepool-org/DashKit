7619a233999ab8b87b8f9ed389a884be6836bcad
Pete Schwamb <pete@schwamb.net>
2019-04-18 19:52:22 -0500

    Add DashKitUI skeleton

---
+++ DashKitUI/DashKitUI.h
@@ -0,0 +1,19 @@
+//
+//  DashKitUI.h
+//  DashKitUI
+//
+//  Created by Pete Schwamb on 4/18/19.
+//  Copyright © 2019 Tidepool. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for DashKitUI.
+FOUNDATION_EXPORT double DashKitUIVersionNumber;
+
+//! Project version string for DashKitUI.
+FOUNDATION_EXPORT const unsigned char DashKitUIVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DashKitUI/PublicHeader.h>
+
+
