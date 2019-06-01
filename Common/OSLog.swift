939374eb730c6b3cb379a18b70dd4477132e2177
Pete Schwamb <pete@schwamb.net>
2019-05-31 21:21:27 -0500

    Issuing boluses working. Doses not being completely tracked yet.

---
+++ Common/OSLog.swift
@@ -0,0 +1,9 @@
+//
+//  OSLog.swift
+//  DashKitUI
+//
+//  Created by Pete Schwamb on 5/31/19.
+//  Copyright © 2019 Tidepool. All rights reserved.
+//
+
+import Foundation
---
+++ DashKit/UnfinalizedDose.swift
@@ -0,0 +1,9 @@
+//
+//  UnfinalizedDose.swift
+//  DashKit
+//
+//  Created by Pete Schwamb on 5/31/19.
+//  Copyright © 2019 Tidepool. All rights reserved.
+//
+
+import Foundation
