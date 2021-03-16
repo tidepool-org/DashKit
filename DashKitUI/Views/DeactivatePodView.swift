c8ef8a3c1e96520eb5bd0612651c4885e832fa72
Pete Schwamb <pete@schwamb.net>
2021-03-16 16:14:44 -0500

    LOOP-1247 Deactivation updates (#104)
    * Updates to deactivation based on new design
    
    * Show modal on discard if pod is attached to body

--- DashKitUI/Views/DeactivatePodView.swift
+++ DashKitUI/Views/DeactivatePodView.swift
@@ -15,6 +15,8 @@ struct DeactivatePodView: View {
 
     @Environment(\.verticalSizeClass) var verticalSizeClass
     @Environment(\.guidanceColors) var guidanceColors
+    
+    @State private var removePodModalIsPresented: Bool = false
 
     var body: some View {
         GuidePage(content: {
@@ -27,50 +29,71 @@ struct DeactivatePodView: View {
                     Spacer()
                 }
             }
+            .padding(.bottom, 8)
         }) {
-            if viewModel.state.showProgressDetail {
-                VStack {
-                    viewModel.error.map {ErrorView($0).accessibility(sortPriority: 0)}
-
-                    if viewModel.error == nil {
-                        VStack {
-                            HStack { Spacer () }
-                            ProgressIndicatorView(state: viewModel.state.progressState)
-                                .padding(.horizontal)
-                            if viewModel.state.isFinished {
-                                FrameworkLocalText("Deactivated", comment: "Label text showing pod is deactivated")
-                                    .bold()
-                                    .padding(.top)
+            VStack {
+                if viewModel.state.showProgressDetail {
+                    VStack {
+                        viewModel.error.map {ErrorView($0).accessibility(sortPriority: 0)}
+                        
+                        if viewModel.error == nil {
+                            VStack {
+                                ProgressIndicatorView(state: viewModel.state.progressState)
+                                if self.viewModel.state.isFinished {
+                                    FrameworkLocalText("Deactivated", comment: "Label text showing pod is deactivated")
+                                        .bold()
+                                        .padding(.top)
+                                }
                             }
+                            .padding(.bottom, 8)
                         }
+
                     }
+                    .transition(AnyTransition.opacity.combined(with: .move(edge: .bottom)))
                 }
-                .transition(AnyTransition.opacity.combined(with: .move(edge: .bottom)))
-                .padding([.top, .horizontal])
+                if viewModel.error != nil {
+                    Button(action: {
+                        if viewModel.podAttachedToBody {
+                            removePodModalIsPresented = true
+                        } else {
+                            viewModel.discardPod()
+                        }
+                    }) {
+                        FrameworkLocalText("Discard Pod", comment: "Text for discard pod button")
+                            .accessibility(identifier: "button_discard_pod_action")
+                            .actionButtonStyle(.destructive)
+                    }
+                    .disabled(viewModel.state.isProcessing)
+                }
+                Button(action: {
+                    viewModel.continueButtonTapped()
+                }) {
+                    Text(viewModel.state.actionButtonDescription)
+                        .accessibility(identifier: "button_next_action")
+                        .accessibility(label: Text(viewModel.state.actionButtonAccessibilityLabel))
+                        .actionButtonStyle(viewModel.state.actionButtonStyle)
+                }
+                .disabled(viewModel.state.isProcessing)
             }
-            Button(action: {
-                viewModel.continueButtonTapped()
-            }) {
-                Text(viewModel.state.actionButtonDescription)
-                    .accessibility(identifier: "button_next_action")
-                    .accessibility(label: Text(viewModel.state.actionButtonAccessibilityLabel))
-                    .actionButtonStyle(viewModel.state.actionButtonStyle)
-            }
-            .disabled(viewModel.state.isProcessing)
-            .animation(nil)
             .padding()
-            .background(Color(UIColor.systemBackground))
-            .zIndex(1)
         }
+        .alert(isPresented: $removePodModalIsPresented) { removePodModal }
         .navigationBarTitle("Deactivate Pod", displayMode: .automatic)
         .navigationBarItems(trailing:
-            viewModel.error != nil ?
-                Button("Discard Pod") {
-                    viewModel.discardPodButtonTapped()
-                }.foregroundColor(guidanceColors.critical) : nil
+            Button("Cancel") {
+                viewModel.didCancel?()
+            }
         )
     }
     
+    var removePodModal: Alert {
+        return Alert(
+            title: FrameworkLocalText("Remove Pod from Body", comment: "Title for remove pod modal"),
+            message: FrameworkLocalText("Your Pod may still be delivering Insulin.\nRemove it from your body, then tap “Continue.“", comment: "Alert message body for confirm pod attachment"),
+            primaryButton: .cancel(),
+            secondaryButton: .default(FrameworkLocalText("Continue", comment: "Title of button to continue discard"), action: { viewModel.discardPod() })
+        )
+    }
 }
 
 struct DeactivatePodView_Previews: PreviewProvider {
