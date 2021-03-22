edacda33dc948d56d961865eec5f0c5bbdcad863
Nathaniel Hamming <nate@hmtconsulting.org>
2021-03-22 13:19:30 -0300

    more readable code

--- DashKitUI/Views/NotificationSettingsView.swift
+++ DashKitUI/Views/NotificationSettingsView.swift
@@ -13,7 +13,7 @@ import HealthKit
 
 struct NotificationSettingsView: View {
     
-    static let expirationReminderHoursAllowed = 1..<25
+    static let expirationReminderHoursAllowed = 1...24
     
     var dateFormatter: DateFormatter
     
