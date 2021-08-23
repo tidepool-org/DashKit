8adc056d8734a1b5ef2e454df69bc7fb1318a567
Nathaniel Hamming <nate@hmtconsulting.org>
2021-08-23 09:53:01 -0300

    corrected alignment of notification settings

--- DashKitUI/Views/NotificationSettingsView.swift
+++ DashKitUI/Views/NotificationSettingsView.swift
@@ -12,7 +12,7 @@ import LoopKitUI
 import HealthKit
 
 struct NotificationSettingsView: View {
-    
+
     var dateFormatter: DateFormatter
     
     @Binding var expirationReminderDefault: Int
@@ -54,12 +54,10 @@ struct NotificationSettingsView: View {
                 lowReservoirValueRow
             }
 
-            VStack(alignment: .leading) {
-                RoundedCardTitle(LocalizedString("Critical Alerts", comment: "Title for critical alerts description"))
-                    .padding(.bottom, 1)
-                RoundedCardFooter(LocalizedString("The reminders above will not sound if your device is in Silent or Do Not Disturb mode.\n\nThere are other critical Pod alerts and alarms that will sound even if you device is set to Silent or Do Not Disturb mode.", comment: "Description text for critical alerts"))
-            }
-            .padding(.horizontal, 16)
+            RoundedCard<EmptyView>(
+                title: LocalizedString("Critical Alerts", comment: "Title for critical alerts description"),
+                footer: LocalizedString("The reminders above will not sound if your device is in Silent or Do Not Disturb mode.\n\nThere are other critical Pod alerts and alarms that will sound even if you device is set to Silent or Do Not Disturb mode.", comment: "Description text for critical alerts")
+            )
         }
         .navigationBarTitle(LocalizedString("Notification Settings", comment: "navigation title for notification settings"))
     }
